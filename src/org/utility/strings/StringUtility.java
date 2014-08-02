b111b8963c26d78fd047f29486e5b83840d12598
BHARAT SAVANI <savanibharat@gmail.com>
2014-08-02 11:04:31 -0700

    String Array to ArrayList and vice versa

--- src/org/utility/strings/StringUtility.java
+++ src/org/utility/strings/StringUtility.java
@@ -1,5 +1,22 @@
 package org.utility.strings;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StringUtility {
 
+	// ArrayList to String Array
+	public static String[] listToStringArray(ArrayList<String> list) {
+		if (list == null || list.size() == 0)
+			return null;
+		return (String[]) list.toArray(new String[0]);
+	}
+
+	// String Array to ArrayList
+	public static ArrayList<String> stringArrayToArrayList(String[] strArray) {
+		if (strArray.length == 0 || strArray == null) {
+			return null;
+		}
+		return new ArrayList<String>(Arrays.asList(strArray));
+	}
 }
